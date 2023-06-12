9629062588aca679cd65fe0ccfc9b2187c00b33d
Filip Koteski <fkote@Filips-MacBook-Pro-2.local>
2023-06-12 20:06:40 +0200

    phase 1

---
+++ index.py
@@ -0,0 +1,62 @@
+import json
+
+from types import SimpleNamespace
+from datetime import datetime
+
+from models import WidgetVisit
+
+fileLocation = input('Please provide the file location: ')
+
+# FIXME:
+fileLocation = 'sample.json'
+# FIXME:
+
+# There is an inconsistency in the docs for the open function. https://docs.python.org/3/library/functions.html#open
+# open() takes a second argument, mode. There are two different parameters marked as default :)
+file = open(fileLocation)
+# Decided on using a list because it's changable and orderable, I'll need to order it using its built in sort func
+logs = []
+
+for data in file:
+    # widgetVisit = json.loads(data) 
+    # {
+    #     "httpRequest": {
+    #         "cacheHit": true,
+    #         "cacheLookup": true,
+    #         "latency": "0.002054s",
+    #         "requestMethod": "GET",
+    #         "requestSize": "204",
+    #         "requestUrl": "https://embedsocial.com/api/pro_hashtag/b4671a7b49a1f8145df2b8c758/",
+    #         "responseSize": "20994",
+    #         "status": 200
+    #     },
+    #     "timestamp": "2023-05-03T00:00:03.567794Z",
+    #     "severity": "INFO"
+    # }
+    # user = json.loads(json_obj, object_hook = User)
+    # print(f"User {user.name}, age {user.age}, email {user.email}")
+    widgetVisit = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+
+    widgetRef = widgetVisit.httpRequest.requestUrl[len('https://embedsocial.com/api/pro_hashtag/'):].split('/')[0].strip()
+    visits_date = datetime.fromisoformat(widgetVisit.timestamp[:-1] + '+00:00')
+
+    widgetVisit = WidgetVisit(
+        1, 
+        'Widget', 
+        widgetRef, 
+        widgetVisit.httpRequest.requestUrl, 
+        visits_date
+    )
+
+    logs.append(widgetVisit)
+
+# Why do I have to use logs.append for appending elements but len(logs) to get the length of the list :(
+print(logs[0])
+
+# Write a command line app with a file name as parameter. Read json objects from the file, and convert them to a DTO. Process this later and write it in the database.
+
+# The goal is to write in the widget_visit table (in the embedsocial db), sorted by date, and increment the number of visits for each widget. The original plan was to use pubsub, but I decided to remove it. It has a steep learning curve and will take a few days to setup (this is better used testing the languages).
+
+# Each entry in the db should be grouped by date, and incremented if it already exists. It's fine to use a sample owner id, but it's better to get the real owner and widget type from the db.
+
+# Follow the code in the MessageHandler in PubSub bundle, and add all necessary constants files and libraries.
---
+++ models.py
@@ -0,0 +1,40 @@
+import datetime
+from typing import Any
+from sqlalchemy import ForeignKey, null
+from sqlalchemy import Text
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "fos_user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r})"
+    
+class WidgetVisit(Base):
+    __tablename__ = "widget_visit"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("fos_user.id"))
+    widget_type: Mapped[str] # can be HashtagAlbum or Widget
+    widget_ref:	Mapped[str]
+    request_url: Mapped[str] = mapped_column(Text)
+    visits: Mapped[int] = mapped_column(default=0)
+    visits_date: Mapped[datetime.datetime]		
+    created_on: Mapped[datetime.datetime]		
+    updated_on: Mapped[datetime.datetime] = mapped_column(default=null)	
+    status: Mapped[bool] = mapped_column(default=null)
+
+    def __init__(self, owner_id, widget_type, widget_ref, request_url, visits_date):
+        self.owner_id = owner_id
+        self.widget_type = widget_type
+        self.widget_ref = widget_ref
+        self.request_url = request_url
+        self.visits_date = visits_date
+
+    def __repr__(self) -> str:
+        return f"WidgetVisit(id={self.id!r}, owner_id={self.owner_id!r}, widget_type={self.widget_type!r}, widget_ref={self.widget_ref!r}, request_url={self.request_url!r}, visits={self.visits!r}, visits_date={self.visits_date!r}, created_on={self.created_on!r}, updated_on={self.updated_on!r}, status={self.status!r})"
